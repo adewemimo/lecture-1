0f03da1a16e2f7cdfca1a4f2408b1765adc78ce2
Seun Soetan <sesoetan@gmail.com>
2021-11-09 14:57:12 -0500

    updates

--- printName.js
+++ printName.js
@@ -1,10 +1,12 @@
-// A script that we can type
+// A script(printName.js) that we can type
 //node printName.js [testName]
+//to run the script
+
 const username = process.argv[2];
 
 function printName(){
     const message = "Welcome to WebDevCore " + username + "!"
-    
+
     console.log(message);
 }
 
